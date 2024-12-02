b61916d25341c9956b94de1c9323e16383917430
Natasha Zywczyk <g00415696@atu.ie>
2024-12-02 18:45:41 +0000

    Added placeholder

---
+++ src/components/Items.js
@@ -0,0 +1,7 @@
+import ListItem from './ListItem';
+
+function Items(props) {
+    //Pass all necessary data from ListItem to Items and render components
+}
+
+export default Items;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import Items from './Items';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Main = () =>
+{
+    //Have this page read in bucklet list items from api 
+    //and reload page whenever an item is deleted
+}
+
+export default Main;

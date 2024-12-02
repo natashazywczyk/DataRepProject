d5afb43fa0ddc93440e9ed4db183c6ddba56714f
Natasha Zywczyk <g00415696@atu.ie>
2024-12-02 18:37:41 +0000

    added placeholder for home

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+        {/*Add Homepage content here, introduce app and its use*/}
+    </div>
+  );
+}
+
+export default Home;

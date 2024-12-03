3a854c37f5dac7f5560f94f3e2a40acf423cd222
Natasha Zywczyk <g00415696@atu.ie>
2024-12-03 14:16:52 +0000

    Added homepage information, explaining app

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,22 @@ const Home = () => {
   return (
     <div>
         {/*Add Homepage content here, introduce app and its use*/}
-        <h1>Hello World!</h1>
+        <h1>Welcome!</h1>
+        <h3>This is my Bucket List App!</h3>
+
+        <br></br>
+        
+        <p>What can you do in this app?
+          Great question!
+          <br></br>
+          <br></br>
+          In this app, you can seamlessly and easily add, delete and edit locations you would like to visit, <br></br>
+          new foods you'd like to try or anything to wish to do in the future.
+          <br></br>
+          <br></br>
+          Be it something as small as starting a new hobby, or something as big as travelling around the world, <br></br><br></br>
+          you can freely add these wishes to the app
+        </p>
     </div>
   );
 }

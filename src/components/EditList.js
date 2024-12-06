49922bdf626d2ef59461f94ffc630fd95f1ce986
Natasha Zywczyk <g00415696@atu.ie>
2024-12-06 09:59:31 +0000

    imported functions and hooks needed

--- src/components/EditList.js
+++ src/components/EditList.js
@@ -3,3 +3,14 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+
+//Import functions and hooks
+export default function Edit(props) {
+    let { id } = useParams();
+    const [title, setTitle] = useState("");
+    const [dateAdd, setDateAdd] = useState("");
+    const [picture, setPicture] = useState("");
+    const [desc, setDescription] = useState("");
+    const navigate = useNavigate(); //Allows navigation to other roots
+
+}

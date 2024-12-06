c7e491dbffe4f856bf504c7771e0e089205fce9f
Natasha Zywczyk <g00415696@atu.ie>
2024-12-06 10:12:31 +0000

    useEffect() added

--- src/components/EditList.js
+++ src/components/EditList.js
@@ -13,6 +13,22 @@ export default function EditList(props) {
     const [desc, setDescription] = useState("");
     const navigate = useNavigate(); //Allows navigation to other roots
 
+    //Do when movie id changes
+    useEffect(() => {
+        //GETS movie from id given
+        axios.get('http://localhost:4000/api/wishes/' + id)
+            .then((response) => {
+                setTitle(response.data.title);
+                setDateAdd(response.data.dateAdd);
+                setPicture(response.data.picture);
+                setDescription(response.data.desc);
+            })
+            //If there's an error
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [id]);
+
     //Allows for only one page to run at a time
     const handleSubmit = (event) => {
         event.preventDefault(); //Don't allow reload to default page
@@ -28,45 +44,44 @@ export default function EditList(props) {
             navigate('/read');
         });
     }
-    
-    
-//Display the movie information
-return (
-    <div>
-        <h3>Edit a wish on your bucket list</h3>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label>Wish Title: </label>
-                <input type="text" 
-                className="form-control" 
-                value={title} 
-                onChange={(e) => setTitle(e.target.value)} />
-            </div>
-            <div className="form-group">
-                <label>Date Added: </label>
-                <input type="text" 
-                className="form-control" 
-                value={dateAdd} 
-                onChange={(e) => setDateAdd(e.target.value)} />
-            </div>
-            <div className="form-group">
-                <label>Description: </label>
-                <input type="text" 
-                className="form-control" 
-                value={desc} 
-                onChange={(e) => setDescription(e.target.value)} />
-            </div>
-            <div className="form-group">
-                <label>Picture URL: </label>
-                <input type="text" 
-                className="form-control" 
-                value={picture} 
-                onChange={(e) => setPicture(e.target.value)} />
-            </div>
-            <div className="form-group">
-                <input type="submit" value="Edit Wish" className="btn btn-primary" />
-            </div>
-        </form>
-    </div>
-);
+
+    //Display the movie information
+    return (
+        <div>
+            <h3>Edit a wish on your bucket list</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Wish Title: </label>
+                    <input type="text" 
+                    className="form-control" 
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Date Added: </label>
+                    <input type="text" 
+                    className="form-control" 
+                    value={dateAdd} 
+                    onChange={(e) => setDateAdd(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Description: </label>
+                    <input type="text" 
+                    className="form-control" 
+                    value={desc} 
+                    onChange={(e) => setDescription(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Picture URL: </label>
+                    <input type="text" 
+                    className="form-control" 
+                    value={picture} 
+                    onChange={(e) => setPicture(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Edit Wish" className="btn btn-primary" />
+                </div>
+            </form>
+        </div>
+    );
 }

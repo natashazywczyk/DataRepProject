f3b9ba83be15ed9e5797e24917292d911ae55135
Natasha Zywczyk <g00415696@atu.ie>
2024-12-06 09:56:44 +0000

    pass data from ListItem and render

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,6 +2,17 @@ import ListItem from './ListItem';
 
 function Items(props) {
     //Pass all necessary data from ListItem to Items and render components
+    return (
+        <>
+            {props.mywishes.map((wish) => (
+                <ListItem
+                    myMovie={wish}
+                    key={wish._id}
+                    Reload={props.ReloadData} //handles reload function
+                />
+            ))}
+        </>
+    );
 }
 
 export default Items;

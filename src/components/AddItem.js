a3d61a4137cdfdadb55af3a8fcb77c78007a167f
Natasha Zywczyk <g00415696@atu.ie>
2024-12-06 09:37:51 +0000

    Allow values to be saved for bucket list item

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -17,6 +17,49 @@ const AddItem = () => {
         .then((res) => console.log(res.data))
         .catch((err) => console.log(err.data));
     }
+
+    return (
+        <div>
+          <h3>Create a wish to add to your bucket list!</h3>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Add Wish Title: </label>
+                  <input type="text"
+                    className="form-control"
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value) }}
+                  />
+              </div>
+              <div className="form-group">
+                <label>Add Date Wish Was Added: </label>
+                  <input type="text"
+                    className="form-control"
+                    value={dateAdd}
+                    onChange={(e) => { setDateAdd(e.target.value) }}
+                  />
+              </div>
+              <div className="form-group">
+                <label>Add Wish Description: </label>
+                  <input type="text"
+                    className="form-control"
+                    value={desc}
+                    onChange={(e) => { setDescription(e.target.value) }}
+                  />
+              </div>
+              <div className="form-group">
+                <label>Add Wish Picture: </label>
+                  <input type="text"
+                    className="form-control"
+                    value={picture}
+                    onChange={(e) => { setPicture(e.target.value) }}
+                  />
+                </div>
+                <div>
+                  <input type="submit" value="Add Movie"></input>
+                </div>
+              </form>
+          </div>
+      );
 }
 
 export default AddItem;

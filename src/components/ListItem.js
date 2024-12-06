a149c2369eeab33c6c19d6a4574adb2cba3cc660
Natasha Zywczyk <g00415696@atu.ie>
2024-12-06 10:23:33 +0000

    handleDelete() added

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -10,6 +10,21 @@ const ListItem = (props) =>
     useEffect(() => {
         console.log("Bucket List Item:", props.mywishes);
     }, [props.mywishes]); // Only run this effect when the myList prop changes
+
+    //Handles the delete request when button is clicked
+    const handleDelete = (e) => 
+    {
+      e.preventDefault();
+  
+      axios.delete('http://localhost:4000/api/wishes/' + props.mywishes._id) //delete wish by id
+      .then((res) => 
+      {
+        props.Reload(); //reload when deleted
+      })
+      .catch((error) => {
+        console.error("Error deleting wish:", error); //handles error if wish couldn't be deleted
+      });
+    }
       
     return (
         <div>

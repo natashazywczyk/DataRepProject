3e8a25bc1f06b01a8e76470dbca1e513ea042193
Natasha Zywczyk <g00415696@atu.ie>
2024-12-06 09:31:35 +0000

    Added UseEffect and Display

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,7 +7,23 @@ import Button from 'react-bootstrap/Button';
 
 const ListItem = () => 
 {
-
+    useEffect(() => {
+        console.log("Bucket List Item:", props.mywishes);
+      }, [props.mywishes]); // Only run this effect when the myList prop changes
+      
+    return (
+        <div>
+          <Card>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <img src={props.mywishes.picture} alt={props.mywishes.title} />
+                <footer>{props.mywishes.dateAdd}</footer>
+                <p>{props.mywishes.desc}</p>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        </div>
+      );
 }
 
 export default ListItem;

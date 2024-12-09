74ceefc3ac680b8e859ffbe3ab46c0ef7eecd82c
Natasha Zywczyk <g00415696@atu.ie>
2024-12-09 22:34:43 +0000

    Added static background colour

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,46 @@ const Home = () => {
           Be it something as small as starting a new hobby, or something as big as travelling around the world, <br></br><br></br>
           you can freely add these wishes to the app
         </p>
+        <br></br>
+        <br></br>
+
+        {/*Images in one row*/}
+        <div className="container mt-6">
+        <div className="row mb-3">
+          <div className="col-md-4 d-flex justify-content-center">
+            <div className="p-1 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: 'transparent' }}>
+              <img
+                src="https://i.natgeofe.com/k/f576c284-661a-4046-ba51-fa95699e1a8b/hawaii-beach.png"
+                alt="beach-img"
+                className="img-fluid" // Makes Image change with div
+                style={{ maxWidth: '100%', height: 'auto' }} // Adjust size if needed
+              />
+            </div>
+          </div>
+
+          <div className="col-md-4 d-flex justify-content-center">
+            <div className="p-1 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: 'transparent' }}>
+              <img
+                src="https://www.irishtimes.com/resizer/3KNQ47rgLHz-sHCd5d_7tRqMjOo=/arc-photo-irishtimes/eu-central-1-prod/public/GI7BDVKOBFEGBI6ZSA4IOLRQM4.jpg"
+                alt="knitting-img"
+                className="img-fluid" // Makes Image change with div
+                style={{ maxWidth: '100%', height: 'auto' }} // Adjust size if needed
+              />
+            </div>
+          </div>
+
+          <div className="col-md-4 d-flex justify-content-center">
+            <div className="p-1 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: 'transparent' }}>
+              <img
+                src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/tasty_gyoza_japanese_16947_16x9.jpg"
+                alt="dumplings-img"
+                className="img-fluid" // Makes Image change with div
+                style={{ maxWidth: '100%', height: 'auto' }} // Adjust size if needed
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
